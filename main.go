bc4998668d23f19ba6eb1d7a330c85ca246732cb
Timothée Pasteau-Berthaud <timothee.pasteau-berthaud@epitech.eu>
2025-02-09 09:47:20 +0100

    update(my_trello): main.go

--- main.go
+++ main.go
@@ -10,5 +10,5 @@ import (
 func main() {    
 	r := router.Router()    
 	fmt.Println("Starting server on the port 8080...")    
-	http.ListenAndServe(":8080", r)
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
