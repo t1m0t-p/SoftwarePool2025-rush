ca9ada3f7e02a3c0172d0df8bf13050e3654e289
Timothée Pasteau-Berthaud <timothee.pasteau-berthaud@epitech.eu>
2025-02-09 09:45:45 +0100

    feat(my_trello): add main

--- main.go
+++ main.go
@@ -2,8 +2,13 @@ package main
 
 import (
     "fmt"
+    "log"
+    "net/http"
+    "./router"
 )
 
 func main() {    
+	r := router.Router()    
 	fmt.Println("Starting server on the port 8080...")    
+	http.ListenAndServe(":8080", r)
 }
